a006bda2fd5d25879b7ac8d081dd82ab0ad36179
amitk2007 <amitk2007@gmail.com>
2016-11-07 13:44:10 +0200

    First Try

---
+++ Assets/Scripts/AdManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using admob;
+
+public class AdManager : MonoBehaviour {
+
+	public static AdManager Instance{ set; get;}
+	public string BunnerID = "ca-app-pub-1819293788939652/2788058490";
+	public string VidoeID = "ca - app - pub - 1819293788939652 / 6380653291";
+	// Use this for initialization
+	private void Start () {
+		Instance = this;
+		DontDestroyOnLoad (gameObject);
+
+		Admob.Instance ().initAdmob (BunnerID,VidoeID);
+		Admob.Instance ().setTesting (true);
+		Admob.Instance ().loadInterstitial ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ShowBunner()
+	{
+		Admob.Instance ().showBannerRelative (AdSize.Banner, AdPosition.BOTTOM_CENTER, 0);
+	}
+	public void ShowVidoe()
+	{
+		if (Admob.Instance().isInterstitialReady()) {
+			Admob.Instance().showInterstitial();
+		}
+	}
+}
---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		AdManager.Instance.ShowBunner ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
