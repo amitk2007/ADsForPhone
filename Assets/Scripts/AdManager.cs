14ed681495359505efe3f877ca3f93d4fb185caa
amitk2007 <amitk2007@gmail.com>
2016-11-07 19:29:28 +0200

    testing ads #3

--- Assets/Scripts/AdManager.cs
+++ Assets/Scripts/AdManager.cs
@@ -7,6 +7,8 @@ public class AdManager : MonoBehaviour {
 	public static AdManager Instance{ set; get;}
 	public string BunnerID = "ca-app-pub-1819293788939652/2788058490";
 	public string VidoeID = "ca-app-pub-1819293788939652/6380653291";
+
+	bool AdsIsTest = true;
 	// Use this for initialization
 	private void Start () {
 
@@ -14,15 +16,24 @@ public class AdManager : MonoBehaviour {
 		DontDestroyOnLoad (gameObject);
 
 		Admob.Instance ().initAdmob (BunnerID,VidoeID);
-		//Admob.Instance ().setTesting (true);
+		if (AdsIsTest) {
+			Admob.Instance ().setTesting (true);
+		}
 		Admob.Instance ().loadInterstitial ();
 
+	}
+
+	void OnGUI(){
+		if (GUI.Button (new Rect (0, 0, 200, 200), AdsIsTest.ToString())) {
+			AdsIsTest = !AdsIsTest;
+		}
 	}
 
 	public void ShowBunner()
 	{
 		Admob.Instance ().showBannerRelative (AdSize.Banner, AdPosition.BOTTOM_CENTER, 0);
 	}
+
 
 	public void ShowVidoe()
 	{
